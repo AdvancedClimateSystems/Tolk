80744cee079043d4e1f24b6c50f2f21125ba9d6d
Tonnis Oosterhoff <tonnisoosterhoff@gmail.com>
2015-11-21 17:02:30 +0100

    #7 Added tests to except clause in Handler.handle().

--- tests/test_server.py
+++ tests/test_server.py
@@ -3,3 +3,34 @@ tests of :class:`tolk.Dispatcher` are executed.
 
 See test coverage if you don't believe me.
 """
+import socket
+
+import pytest
+import errno
+from mock import Mock, patch
+from tolk import Handler
+
+
+class TestHandler:
+    def test_handle_raise_socket_error(self):
+        """
+        Test if socket error has been re-raised when `errno` was
+        different than `errno.EPIPE`.
+
+        """
+        mock_request = Mock()
+        mock_request.sendall = Mock(side_effect=socket.error())
+
+        with pytest.raises(socket.error) as exinfo:
+            Handler(mock_request, Mock(), Mock()).handle()
+
+
+    def test_handle_epipe_socket_error(self):
+        """
+        Test if None has been returned when 'errno' was `errno.EPIPE`.
+
+        """
+        mock_request = Mock()
+        mock_request.sendall = Mock(side_effect=socket.error(errno.EPIPE, 'Raise socket error EPIPE'))
+
+        assert Handler(mock_request, Mock(), Mock()).handle() is None
